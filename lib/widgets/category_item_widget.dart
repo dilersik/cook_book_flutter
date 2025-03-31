4b16e835390f18c3a55f0b89eacc80eab7a11370
Dilermando Augusto Sikora <di_ler@hotmail.com>
2025-03-31 15:26:22 -0300

    181. Componente Item Categoria

--- lib/widgets/category_item_widget.dart
+++ lib/widgets/category_item_widget.dart
@@ -2,16 +2,23 @@ import 'package:cook_book_flutter/models/category.dart';
 import 'package:flutter/material.dart';
 
 class CategoryItem extends StatelessWidget {
-
   final Category category;
 
   const CategoryItem({super.key, required this.category});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
+      padding: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        gradient: LinearGradient(
+          colors: [category.color.withValues(alpha: 0.7), category.color],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
       child: Text(category.title),
     );
   }
-
 }
