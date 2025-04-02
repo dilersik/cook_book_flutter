d0c185084bf2b3c58750e3b386ed11ddebed6429
Dilermando Augusto Sikora <di_ler@hotmail.com>
2025-04-02 17:09:02 -0300

    194. Tela Detalhe Refeição #02

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -7,18 +7,45 @@ class MealDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final meal = ModalRoute.of(context)!.settings.arguments as Meal;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(meal.title),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text("Meal Detail Screen"),
+      appBar: AppBar(title: Text(meal.title), centerTitle: true),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 300, width: double.infinity, child: Image.network(meal.imageUrl, fit: BoxFit.cover)),
+            _createSectionTitle(context, "Ingredients"),
+            ListView.builder(
+              shrinkWrap: true, // Makes the ListView take only the necessary space
+              physics: NeverScrollableScrollPhysics(), // Prevents conflict with the parent ScrollView
+              itemCount: meal.ingredients.length,
+              itemBuilder: (ctx, index) {
+                return Card(
+                  color: Theme.of(context).colorScheme.secondary,
+                  child: Padding(padding: const EdgeInsets.all(10), child: Text(meal.ingredients[index])),
+                );
+              },
+            ),
+            _createSectionTitle(context, "Steps"),
+            ListView.builder(
+              shrinkWrap: true, // Makes the ListView take only the necessary space
+              physics: NeverScrollableScrollPhysics(), // Prevents conflict with the parent ScrollView
+              itemCount: meal.steps.length,
+              itemBuilder: (ctx, index) {
+                return ListTile(leading: CircleAvatar(child: Text('# ${index + 1}')), title: Text(meal.steps[index]));
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
 
+  Widget _createSectionTitle(BuildContext context, String title) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: Text(title, style: Theme.of(context).textTheme.titleLarge),
+    );
+  }
 }
